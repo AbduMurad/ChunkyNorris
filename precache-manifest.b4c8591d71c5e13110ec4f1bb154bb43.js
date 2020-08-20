863c6b17d7ddb0e77efc56876d31edcb506a0d2d
AbduMurad <abadeem@gmail.com>
2020-08-20 16:49:47 +0200

    Updates

---
+++ precache-manifest.b4c8591d71c5e13110ec4f1bb154bb43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2672de55892650d0297c376eb4fd2260",
+    "url": "/ChunkyNorris/index.html"
+  },
+  {
+    "revision": "2be846795ae85d4a60b1",
+    "url": "/ChunkyNorris/static/css/main.3fc3ccb9.chunk.css"
+  },
+  {
+    "revision": "bd6c18c802421e245bad",
+    "url": "/ChunkyNorris/static/js/2.ba698c03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChunkyNorris/static/js/2.ba698c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be846795ae85d4a60b1",
+    "url": "/ChunkyNorris/static/js/main.eba20e31.chunk.js"
+  },
+  {
+    "revision": "6708a5eae34e567b0326",
+    "url": "/ChunkyNorris/static/js/runtime-main.3a7c63f9.js"
+  }
+]);
